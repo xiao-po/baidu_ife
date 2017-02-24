ca5dfafef2cf0817ac21bffb4e30f1755d1a957d
xiao-po <wly19960911@gmail.com>
2017-02-24 20:26:22 +0800

    更改数据绑定第二章错误

---
+++ 动态数据绑定/2.js
@@ -0,0 +1,37 @@
+var Observer = function(object) {
+	for(let key in object){
+		this[key] = object[key];
+		if (object.hasOwnProperty(key)) {
+            val = object[key];
+            // 这里进行判断，如果还没有遍历到最底层，继续new Observer
+            if (typeof val === 'object') {
+                new Observer(val);
+            }
+            
+        }
+	}
+	this.convert = function(key,val,callback){
+		Reflect.defineProperty(this, key, {
+			enumerable: true,
+        	configurable: true,
+        	get: function () {
+        	    return val
+        	},
+        	set: function(newVal) {
+        		callback(newVal);        		
+	            if (newVal === val) return;
+	            val = newVal
+        	} 
+        })
+	}
+    this.$watch = function(key, callback) {
+    	this.convert(key, this[key], callback);
+       
+    }
+}
+
+var app1 = new Observer();
+app1.$watch('age', function(age) {
+         console.log(`我的年纪变了，现在已经是：${age}岁了`)
+ });
+app1.age = 1
