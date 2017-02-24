bd562d8e5dbc7b563f6dec5bed12086304f6bf13
xiao-po <wly19960911@gmail.com>
2017-02-24 13:17:33 +0800

    动态数据绑定学习

---
+++ 动态数据绑定/1.js
@@ -0,0 +1,21 @@
+var Observer = function(object) {
+	if(typeof object === 'object')
+	    return new Proxy(object, {
+	        get: function(target, key, receiver) {
+	            console.log('你访问了 ' + key);
+	            return Reflect.get(target, key, receiver);
+	        },
+	        set: function(target, key, value, receiver) {
+	            console.log('你访问了 ' + key + ',新的值为 ' + value);
+	            return Reflect.set(target, key, value, receiver);
+	        }
+	    })
+	else{
+		return '参数非对象';
+	}
+}
+
+var app1 = new Observer({
+    name: 'xiaopo',
+    age: 20
+})
