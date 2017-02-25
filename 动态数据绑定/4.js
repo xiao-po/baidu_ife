f9833d21ff9b411ac6f91a01df83af37e894de6c
xiao-po <wly19960911@gmail.com>
2017-02-25 14:50:19 +0800

    更新第四章

--- 动态数据绑定/4.js
+++ 动态数据绑定/4.js
@@ -18,8 +18,9 @@ var Observer = function(object) {
     var temp;
     var Rex = /\{\{(.+?)\}\}/g;
     var self = this;
+    //节点分析，通过el值去解析然后把dom返回的node值里面
     var complier = function() {
-            let str = /^[A-Za-z]+$/;
+            //id class 以及tag解析
             if (self.el[0] === '#') {
                 let idname = self.el.slice(1);
                 self.node = document.getElementById(idname);
@@ -33,22 +34,24 @@ var Observer = function(object) {
             if (self.node === null) {
                 throw '节点不存在';
             }
+            //把到时候需要改变的值放在temp里面
             temp = self.node.innerHTML;
         }
-        // var replace = function(Rex){
-        // 	self.node.innerHTML.replace(Rex,self.data[]);
-        // }
+    //分析{{}}表达式里面内容并提取出来，目前的格式是'{{name.fn}}'能解析成数组['name','fn']
     var parseTextExp = function(text) {
         var regText = /\{\{(.+?)\}\}/g;
         var regText1 = /(\w+)+?/g;
         return text.match(regText)[0].match(regText1);
     }
+    //替换，temp的作用是为了防止多次替换不好提取值或者冲突先使用的缓存，
+
     var replace = function(temp) {
         let str = parseTextExp(temp);
         let obj = self.data;
 
         test = obj;
         test1 = str;
+    	//把对应data对象里面的值取出来
         for (var i = 0; i <= str.length - 1; i++) {
             obj = obj[str[i]];
             if (i === 0)
@@ -56,10 +59,10 @@ var Observer = function(object) {
             else
                 objname += str[i] + '.';
         }
-        objname = objname.slice(0, objname.length - 1)
 
+        objname = objname.slice(0, objname.length - 1)
+        //至此都是替换
         let tempStr = temp.replace('{{' + objname + '}}', obj)
-        console.log(temp + '  ' + objname + '   ' + obj + '   ' + tempStr)
 
         self.node.innerHTML = tempStr;
     }
